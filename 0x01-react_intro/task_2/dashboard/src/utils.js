f0671293cab6546681aa4be27f76e862a328442e
Uchechukwu Ekemezie <ucheemekaekemezie@gmail.com>
2023-11-05 18:35:13 +0100

    more react components

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -2,3 +2,7 @@ export const getFullYear = () => new Date().getFullYear();
 
 export const getFooterCopy = (isIndex) =>
   isIndex ? "Holberton School" : "Holberton School main dashboard";
+
+export const getLatestNotification = () => {
+  return "<strong>Urgent requirement</strong> - complete by EOD";
+};
